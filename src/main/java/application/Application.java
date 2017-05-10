8d0d414798b7ffd2639e2860405724ae15b4cec9
Sanyam96 <sanyam.bvcoe96@gmail.com>
2017-05-11 04:06:38 +0530

    Application file added

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,17 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by sanyam on 11/5/17.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/application/Controller/ApacheController.java
@@ -0,0 +1,14 @@
+package application.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by sanyam on 11/5/17.
+ */
+
+@RestController
+public class ApacheController {
+
+
+
+}
